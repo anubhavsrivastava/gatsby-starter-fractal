f43ec8318e4be66d3a6cc14546810b8baf78071e
Anubhav Srivastava <anubhav.srivastava00@gmail.com>
2019-06-15 12:41:16 +0530

    added config for site

---
+++ config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Fractal', // <title>
+  manifestName: 'Fractal',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-fractal/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Anubhav Srivastava',
+  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:anubhav.srivastava00@gmail.com',
+    },
+  ],
+  phone: '000-00000',
+  address: '305 Del Sol, Sunnyvale, CA',
+};
